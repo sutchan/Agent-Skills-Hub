15a4dc16ac02ace1c20de678c0e6a94e1cfca432
T5820 <t5820@skills-manager.local>
2026-07-31 22:27:37 +0800

    清理

--- skills/git-askpass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Managed by Skills Manager. Supplies git credentials from the environment.
-case "$1" in
-*[Uu]sername*) printf '%s\n' "${SKILLS_MANAGER_ASKPASS_USERNAME}" ;;
-*) printf '%s\n' "${SKILLS_MANAGER_ASKPASS_PASSWORD}" ;;
-esac
